1b0ca4bad5fe1c0867ebc4a9939360e7fe4b0142
musen <2438777434@qq.com>
2019-09-02 17:31:43 +0800

    algorithms 算法

--- src/main/java/algorithms/BubbleSort.java
+++ src/main/java/algorithms/BubbleSort.java
@@ -5,15 +5,11 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) throws Exception {
-        int[] arr = new int[3];
-        arr[0]=3;
-        arr[1]=2;
-        arr[2]=1;
-        System.out.print(sort(arr));;
+        int[] arr = {2,5,6,8,4,15,58,4,5,52,7};
+        System.out.print(Arrays.toString(sort(arr)));;
     }
 
     public static  int[] sort(int[] sourceArray) throws Exception {
-        // 321
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
         for (int i = 1; i < arr.length; i++) {
             // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
